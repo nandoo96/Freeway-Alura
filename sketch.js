3888cf0e6b6994a8eececbe5dd201c3cf990770e
Fernando Silva <101579864+nandoo96@users.noreply.github.com>
2022-05-05 07:38:42 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  createCanvas(500, 400);
+  somDaTrilha.loop(0,1,0.02);
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+}
+
